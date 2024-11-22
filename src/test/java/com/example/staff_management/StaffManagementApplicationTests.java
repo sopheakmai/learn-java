7b7268ac64a30a09ae3c7050b1365f390f211278
sopheakmai <sopheak.mai90@gmail.com>
2024-11-22 22:39:59 +0700

    first commit

---
+++ src/test/java/com/example/staff_management/StaffManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staff_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
